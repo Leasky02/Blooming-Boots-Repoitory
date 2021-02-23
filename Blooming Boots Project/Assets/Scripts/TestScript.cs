2ecfc3ea260f2a886580253f035ea82ba05d3bc7
Leasky02 <adleask@hotmail.co.uk>
2021-02-23 17:36:25 +0000

    Created first script to test score
    applied first script to test objects. the script creates a score which increases constantly and is reset at 1000.

---
+++ Blooming Boots Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    //variables thatll be used in unity
+    public string startText;
+    public int score;
+    public int repeatedScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(startText);
+        score = 0;
+        repeatedScore = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += 1;
+        if(score==1000)
+        {
+            Debug.Log("Score reached 1000 x" + repeatedScore);
+            score = 0;
+            repeatedScore += 1;
+        }
+        else
+        {
+            Debug.Log("Testing score");
+        }
+
+    }
+}
