3b857f7475801beefe031ab7837f1b224f208d73
Leasky02 <adleask@hotmail.co.uk>
2021-04-13 19:46:25 +0100

    added combat
    added enemies, health bar and a 3rd level into the game.

---
+++ Blooming Boots Project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth;
+    int currentHealth;
+    public string levelToLoad;
+
+    void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void ChangeHealth(int changeAmount)
+    {
+        currentHealth = currentHealth + changeAmount;
+        //keep health between 0 and starting health
+        currentHealth = Mathf.Clamp(currentHealth, 0, startingHealth);
+
+        if(currentHealth == 0)
+        {
+            Kill();
+        }
+    }
+
+    //function that will be called when a collision occurs
+    public void Kill()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public int GetHealth()
+    {
+        return currentHealth;
+    }
+}
--- Blooming Boots Project/Assets/Scripts/SignPostLeftScript.cs
+++ Blooming Boots Project/Assets/Scripts/SignPostLeftScript.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SignPostRightScript : MonoBehaviour
+public class SignPostLeftScript : MonoBehaviour
 {
     public string levelToLoad;
 
     private void OnTriggerEnter2D(Collider2D otherCollider)
     {
 
-        if(otherCollider.tag == "Player")
+        if (otherCollider.tag == "Player")
             SceneManager.LoadScene(levelToLoad);
     }
 }
